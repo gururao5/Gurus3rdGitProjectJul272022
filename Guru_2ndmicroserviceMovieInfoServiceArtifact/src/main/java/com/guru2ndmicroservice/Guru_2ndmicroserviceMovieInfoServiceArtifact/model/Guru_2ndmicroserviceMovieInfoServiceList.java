acf84baba6cf1e90aadfcc690065c3334c8b53e0
unknown <grao5@hotmail.com>
2022-08-01 15:23:59 -0400

    Adding new movieInfoServiceList class Guru_2ndmicroserviceMovieInfoServiceList.java as part of the microservice whose private member is a list of objects of the other model class Guru_2ndmicroserviceMovieInfoService

---
+++ Guru_2ndmicroserviceMovieInfoServiceArtifact/src/main/java/com/guru2ndmicroservice/Guru_2ndmicroserviceMovieInfoServiceArtifact/model/Guru_2ndmicroserviceMovieInfoServiceList.java
@@ -0,0 +1,22 @@
+package com.guru2ndmicroservice.Guru_2ndmicroserviceMovieInfoServiceArtifact.model;
+
+import java.util.List;
+
+public class Guru_2ndmicroserviceMovieInfoServiceList {
+
+	private List<Guru_2ndmicroserviceMovieInfoService> movieinfoserviceList;
+
+	public Guru_2ndmicroserviceMovieInfoServiceList(List<Guru_2ndmicroserviceMovieInfoService> movieinfoserviceList) {
+		super();
+		this.movieinfoserviceList = movieinfoserviceList;
+	}
+
+	public List<Guru_2ndmicroserviceMovieInfoService> getMovieinfoserviceList() {
+		return movieinfoserviceList;
+	}
+
+	public void setMovieinfoserviceList(List<Guru_2ndmicroserviceMovieInfoService> movieinfoserviceList) {
+		this.movieinfoserviceList = movieinfoserviceList;
+	}
+	
+}
